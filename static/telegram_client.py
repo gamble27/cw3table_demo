cc32044f2c35908640f2223e272394e06a837672
olga <olyushkam@gmail.com>
2019-11-01 20:46:38 +0200

    Telegram API Client update
    + messages with markdown
    + documented code

--- static/telegram_client.py
+++ static/telegram_client.py
@@ -8,11 +8,23 @@ class APIClientError(BaseException):
 
 class TelegramAPIClient:
     def __init__(self, api=""):
+        """
+        Initializes a client for telegram bot
+        :param api: telegram API key of your bot
+        """
         if not api:
             raise APIClientError("no api detected")
+
+        # part of url for bot api commands via POST http method
         self.url = "https://api.telegram.org/bot{API}/".format(API=api)
 
     def send_message(self, chat_id, text="Hello World"):
+        """
+        Sends an ordinary text message
+        :param chat_id: id of recipient
+        :param text: message text
+        :return: None
+        """
         if not chat_id:
             raise APIClientError("no chat id defined")
         parameters = {
@@ -21,7 +33,29 @@ class TelegramAPIClient:
         }
         requests.post(self.url + "sendMessage", json=parameters)
 
+    def send_message_with_markdown(self, chat_id, text="Hello World", markdown="Markdown"):
+        """
+        Sends a message with markdown chosen
+        :param chat_id:  id of recipient
+        :param text: message text
+        :param markdown: markdown type, can be "Markdown" or "HTML"
+        :return: None
+        """
+        if not chat_id:
+            raise APIClientError("no chat id defined")
+        parameters = {
+            "chat_id":      chat_id,
+            "text":         text,
+            "parse_mode":   markdown
+        }
+        requests.post(self.url + "sendMessage", json=parameters)
+
     def get_updates(self):
+        """
+        performs getUpdates API method
+        :return: JSON dictionary containing
+                 list of recent updates
+        """
         response = requests.get(self.url + "getUpdates")
         update = response.json(encoding="utf-8")
         try:
@@ -32,6 +66,11 @@ class TelegramAPIClient:
             return e
 
     def set_webhook(self, host):
+        """
+        Sets a web hook on given SSL protected server
+        :param host: protected https server
+        :return: None
+        """
         if host:
             requests.post(self.url + 'setWebhook?url={}'.format(host))
         else:
